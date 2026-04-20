b6ba3355a4e011c79e47cebc6422ece8fc937586
Agstgrss <augustograssi0101@gmail.com>
2026-04-20 14:45:32 -0300

    adjustment in login controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
 
 Route::get('/', [SiteController::class, 'index'])->name('home');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
