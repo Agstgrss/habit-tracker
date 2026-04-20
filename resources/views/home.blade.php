982e771ea05c3de4da66ad25fbf0b1295623936f
Agstgrss <augustograssi0101@gmail.com>
2026-04-20 13:51:37 -0300

    resources

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,5 @@
-<!doctype html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>
-        {{ config('app.name') }}
-    </title>
-
-    @vite('resources/css/app.css')
-</head>
-
-<body>
-    <h1 class="text-3xl font-bold underline text-green-500">
-        Hello world!
-    </h1>
-</body>
-</html>
+<x-layout>
+    <main>
+        <h1>Veja seus habitos ganharem vida</h1>
+    </main>
+</x-layout>
